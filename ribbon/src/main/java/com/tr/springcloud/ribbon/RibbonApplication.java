4117846089e08eef5ec6648fea429a20fda4a5f4
Air-TR <tr1838@163.com>
2020-08-20 18:05:54 +0800

    get url

--- ribbon/src/main/java/com/tr/springcloud/ribbon/RibbonApplication.java
+++ ribbon/src/main/java/com/tr/springcloud/ribbon/RibbonApplication.java
@@ -1,5 +1,6 @@
 package com.tr.springcloud.ribbon;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,6 +11,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.context.support.HttpRequestHandlerServlet;
+
+import javax.servlet.http.HttpServletRequest;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -19,6 +23,9 @@ public class RibbonApplication {
     public static void main(String[] args) {
         SpringApplication.run(RibbonApplication.class, args);
     }
+
+    @Autowired
+    HttpServletRequest request;
 
     @Bean
     @LoadBalanced
@@ -33,7 +40,18 @@ public class RibbonApplication {
     String serviceName;
 
     @GetMapping("/goods/pay")
-    public String goodsApi() {
+    public String goodsApi(String serviceName) {
+        //方法1.注入HttpServletRequest拿到地址，根据地址来判断访问哪一个服务.
+        String url = "";
+        url = request.getScheme() +"://" + request.getServerName()
+                + ":" +request.getServerPort()
+                + request.getServletPath();
+        if (request.getQueryString() != null){
+            url += "?" + request.getQueryString();
+        }
+        //方法2.将服务的名称作为参数传入到url中，在方法中获取到，通过@RequestParam拿到
+        //此方法需要url为 http://runtao.exigengroup.com:8101/goods/pay?serviceName=pay
+        String name = serviceName;
         return restTemplate().getForObject("http://goods-service/goods/pay", String.class);
     }
 
